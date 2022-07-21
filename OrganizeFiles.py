69f946c58a1f36ddc3618e77e574d5d6cf203795
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-21 17:31:49 +0900

    パス指定方法の修正

--- OrganizeFiles.py
+++ OrganizeFiles.py
@@ -6,7 +6,7 @@ def OrganizeFiles():
     if len(os.listdir("../images/")) > 10:
         files = []
         for file in os.listdir("../images/"):
-            files.append([file, os.path.getctime(file)])
+            files.append([file, os.path.getctime("../images/"+file)])
         files.sort(key=itemgetter(1), reverse=True)
         for i, file in enumerate(files):
             if i > 9:
