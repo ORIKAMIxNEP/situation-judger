7220a5490dd7a8e69d3edc086b634310fcd730ad
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-21 17:28:15 +0900

    定期ファイル削除プログラムを作成

---
+++ OrganizeFiles.py
@@ -0,0 +1,13 @@
+from operator import itemgetter
+import os
+
+
+def OrganizeFiles():
+    if len(os.listdir("../images/")) > 10:
+        files = []
+        for file in os.listdir("../images/"):
+            files.append([file, os.path.getctime(file)])
+        files.sort(key=itemgetter(1), reverse=True)
+        for i, file in enumerate(files):
+            if i > 9:
+                os.remove(file[i])
