eea1b408b627e56efd66e1174bb9fa0f67cdc0e8
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-21 17:58:02 +0900

    updateに変更

--- SituationJudgment.py
+++ SituationJudgment.py
@@ -7,7 +7,7 @@ def SituationJudgment(caption):
     SituationData = dict(caption=caption)
 
     nouns = ExtractNouns(caption)
-    SituationData = dict(nouns=str(nouns))
+    SituationData.update(dict(nouns=str(nouns)))
 
     similarityList = [{"食事中": {"food": 0, "dish": 0, "meal": 0, "snack": 0}}, {"観光中(建物)": {"building": 0, "structure": 0}}, {
         "観光中(風景)": {"landscape": 0, "scene": 0}}, {"動物と触れ合い中": {"animal": 0}}, {"自撮り中、又は他人を撮影中": {"human": 0, "person": 0, "people": 0}}]
@@ -29,8 +29,8 @@ def SituationJudgment(caption):
     print("\n"+str(sums))
 
     if collections.Counter(counts.values())[max(counts.values())] == 1:
-        SituationData = dict(situation=max(counts, key=counts.get))
+        SituationData.update(dict(situation=max(counts, key=counts.get)))
     else:
-        SituationData = dict(situation=max(sums, key=sums.get))
+        SituationData.update(dict(situation=max(sums, key=sums.get)))
     print("\n状況説明："+SituationData["situation"]+"\n")
     return SituationData
