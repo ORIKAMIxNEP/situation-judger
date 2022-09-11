e990cbf9f026f1347c903a7fa7170be76e055714
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-11 22:30:09 +0900

    postTest

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def API():
+    if request.method == "POST":
+        fs = request.files["image"]
+        imagePath = "../images/" + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f") + ".jpg"
+        fs.save(imagePath)
+        print("ok")
     return "test"
 
 
