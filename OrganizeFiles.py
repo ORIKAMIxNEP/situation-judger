10fd004e9f17fbb023016c79bd5646b6946563ab
Taiki Orikami <65590892+ORIKAMIxNEP@users.noreply.github.com>
2022-12-12 14:18:59 +0900

    Update OrganizeFiles.py

--- OrganizeFiles.py
+++ OrganizeFiles.py
@@ -3,11 +3,11 @@ from operator import itemgetter
 
 
 def OrganizeFiles():
-    if len(os.listdir("../images/")) > 10:
+    if len(os.listdir("static/images")) > 10:
         files = []
-        for file in os.listdir("../images/"):
-            files.append([file, os.path.getctime("../images/"+file)])
+        for file in os.listdir("static/images"):
+            files.append([file, os.path.getctime("static/images/"+file)])
         files.sort(key=itemgetter(1), reverse=True)
         for i, file in enumerate(files):
             if i > 9:
-                os.remove("../images/"+file[0])
+                os.remove("static/images/"+file[0])
